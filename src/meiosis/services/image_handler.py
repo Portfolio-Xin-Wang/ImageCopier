81440e43c86e6625d70dc72512d9512dbeacfda9
Xin Wang <Xin20Wang@outlook.com>
2025-12-04 11:27:56 +0100

    Updated version

--- src/meiosis/services/image_handler.py
+++ src/meiosis/services/image_handler.py
@@ -8,14 +8,10 @@ class ImageHandler:
     Main entry point for image copier.
     """
     output_direction: str
-    transformed_name: str
     image_repository: IStorage
     transformers: ImageTransformer
-    _angle: int
 
     def __init__(self, image_repo: IStorage, transformers: ImageTransformer) -> None:
-        self.transformed_name = "{label_id}&{rotation};{file}.{extension}"
-        self._angle = 0
         self.transformers = transformers
         self.image_repository = image_repo
 
@@ -27,4 +23,4 @@ class ImageHandler:
         images: ImageFrame = self.image_repository.get()
         # Export function: format metadata
         result = self.transformers.transform(images)
-        return result.images_collection 
+        return result
