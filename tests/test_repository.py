ccd5a0ddac402161d034aaafe8f2cb50cb02f020
Xin Wang <Xin20Wang@outlook.com>
2025-12-05 16:20:39 +0100

    Upload images for test purposes

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -2,10 +2,10 @@ from pytest import raises
 from src.meiosis import LocalFileStorage, BreadMapper
 
 def test_if_entities_return_only_images():
-    repo = LocalFileStorage("tests/test_images/retrieval", BreadMapper())
+    repo = LocalFileStorage("tests/images/retrieval", BreadMapper())
     image_frame = repo.get()
     assert image_frame.length == 2
 
 def test_if_error_is_raised_on_invalid_datasource():
     with raises(Exception):
-        LocalFileStorage("tests/test_images/non_existent", BreadMapper())
+        LocalFileStorage("tests/images/non_existent", BreadMapper())
