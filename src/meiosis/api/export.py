4fd883f33be8706cd615dfa530e830babdb1bc67
Xin Wang <Xin20Wang@outlook.com>
2025-12-11 16:39:00 +0100

    Made mapping optional in the API

--- src/meiosis/api/export.py
+++ src/meiosis/api/export.py
@@ -1,5 +1,5 @@
 from ..services.meiosis_factory import ImageServiceFactory
-from ..services.mapping import ImageMapper
+from ..services.transformers import MapTransformer
 from ..Domain import ImageFrame
 
 class Export:
@@ -9,6 +9,8 @@ class Export:
     def __init__(self):
         self.image_service_factory = ImageServiceFactory()
 
-    def read_from_directory(self, original_dir="test_images", output_dir="output", copies=3, rotation_base=5) -> ImageFrame:
-        exporter = self.image_service_factory.create_pil_image_copier(copies, rotation_base, original_dir, output_dir)
+    def read_from_directory(self, original_dir="test_images", output_dir="output", copies=3, rotation_base=5, custom_mapper: MapTransformer=None) -> ImageFrame:
+        exporter = self.image_service_factory.create_pil_image_copier(copies, rotation_base, original_dir, output_dir, custom_mapper)
         return exporter.export()
+    
+    
